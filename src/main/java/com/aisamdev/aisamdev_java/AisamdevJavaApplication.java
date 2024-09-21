e74e2557adbbb2f1027b85faba82296f5999e13e
Aisamuddin <aisam0094@gmail.com>
2024-09-21 14:10:36 +0800

    Initial commit

---
+++ src/main/java/com/aisamdev/aisamdev_java/AisamdevJavaApplication.java
@@ -0,0 +1,13 @@
+package com.aisamdev.aisamdev_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisamdevJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisamdevJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aisamdev/aisamdev_java/AisamdevJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisamdev.aisamdev_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisamdevJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
